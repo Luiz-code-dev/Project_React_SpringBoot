a826b09a7e5201980e9a2d246f1c7f2c834a6e88
Luiz Eduardo <programadorlesd@gmail.com>
2021-09-11 20:24:13 -0300

    Criando Projeto React_SpringBoot

---
+++ backend/src/main/java/com/vendason/vendasonline/VendasonlineApplication.java
@@ -0,0 +1,13 @@
+package com.vendason.vendasonline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VendasonlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VendasonlineApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/vendason/vendasonline/VendasonlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vendason.vendasonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VendasonlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
