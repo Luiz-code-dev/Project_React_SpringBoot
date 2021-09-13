9a4e322808de048415d4977fc3829c8998140721
Luiz Eduardo <programadorlesd@gmail.com>
2021-09-12 22:27:27 -0300

    Home, navigation

--- frontend/src/components/NavBar/index.tsx
+++ frontend/src/components/NavBar/index.tsx
@@ -1,4 +1,5 @@
 import ecommerce from 'assets/img/ecommerce.png'
+import { Link } from 'react-router-dom';
 
 
 function NavBar() {
@@ -6,7 +7,9 @@ function NavBar() {
         <div className="d-flex flex-column flex-md-row align-items-center p-3 px-md-4 mb-3 bg-light border-bottom shadow-sm">
             <div className="container">
                 <nav className="my-2 my-md-0 mr-md-3">
+                    <Link to="/">
                     <img src={ecommerce} alt="E-Commerce" width="120" />
+                    </Link>
                 </nav>
             </div>
         </div>
